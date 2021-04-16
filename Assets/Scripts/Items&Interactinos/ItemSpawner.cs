7e1e0eaa8710e3cb63448e29c57020fd19b40431
Pelle Bruinsma <Spookezatemyneighbours@gmail.com>
2021-04-16 12:08:48 +0200

    Coconuts spawn

--- Assets/Scripts/Items&Interactinos/ItemSpawner.cs
+++ Assets/Scripts/Items&Interactinos/ItemSpawner.cs
@@ -12,25 +12,42 @@ public class ItemSpawner : NetworkBehaviour
 
     private void Start()
     {
-        //get spawn positions
-        List<Transform> spawnPositions = new List<Transform>();
-
-        foreach(Transform child in transform)
+        if(transform.childCount > 0)
         {
-            spawnPositions.Add(child);
+            //get spawn positions
+            List<Transform> spawnPositions = new List<Transform>();
+
+            foreach (Transform child in transform)
+            {
+                spawnPositions.Add(child);
+            }
+
+            spawnPositions.OrderBy(s => Random.value);
+
+            //spread items over points
+            int spawns = Random.Range(minMaxSpawns.x, minMaxSpawns.y);
+            for (int s = 0; s < spawns; s++)
+            {
+                GameObject objectToSpawn = spawnableObjects[Random.Range(0, spawnableObjects.Length)];
+
+                //spawn
+                Vector3 offset = Random.insideUnitSphere;
+                NetworkServer.Spawn(Instantiate(objectToSpawn, spawnPositions[Mathf.RoundToInt(Mathf.Repeat(s, spawnPositions.Count))].position + new Vector3(spawnRange.x * offset.x, spawnRange.y * offset.y, spawnRange.z * offset.z), Quaternion.identity));
+            }
+        }
+        else
+        {
+            int spawns = Random.Range(minMaxSpawns.x, minMaxSpawns.y);
+            for (int s = 0; s < spawns; s++)
+            {
+                GameObject objectToSpawn = spawnableObjects[Random.Range(0, spawnableObjects.Length)];
+
+                //spawn
+                Vector3 offset = Random.insideUnitSphere;
+                NetworkServer.Spawn(Instantiate(objectToSpawn, transform.position + new Vector3(spawnRange.x * offset.x, spawnRange.y * offset.y, spawnRange.z * offset.z), Quaternion.identity));
+            }
         }
 
-        spawnPositions.OrderBy(s => Random.value);
-
-        //spread players over teams
-        int spawns = Random.Range(minMaxSpawns.x, minMaxSpawns.y);
-        for (int s = 0; s < spawns; s++)
-        {
-            GameObject objectToSpawn = spawnableObjects[Random.Range(0, spawnableObjects.Length)];
-
-            //spawn
-            Vector3 offset = Random.insideUnitSphere;
-            NetworkServer.Spawn(Instantiate(objectToSpawn, spawnPositions[Mathf.RoundToInt(Mathf.Repeat(s, spawnPositions.Count))].position + new Vector3(spawnRange.x * offset.x, spawnRange.y * offset.y, spawnRange.z * offset.z), Quaternion.identity));
-        }
+        Destroy(this);
     }
 }
