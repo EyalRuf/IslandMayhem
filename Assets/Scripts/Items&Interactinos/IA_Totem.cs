e7c9b2cf784b764fc92d588f37dc947939cab4bf
Pelle Bruinsma <Spookezatemyneighbours@gmail.com>
2021-04-20 14:03:05 +0200

    Added totems

--- Assets/Scripts/Items&Interactinos/IA_Totem.cs
+++ Assets/Scripts/Items&Interactinos/IA_Totem.cs
@@ -11,6 +11,7 @@ public class IA_Totem : InteractionArea
     public int currentVisualStage;
     public bool maxVisualStageReached;
     public GameObject[] visuals;
+    public GameObject smokePoof;
 
     private void Start()
     {
@@ -35,13 +36,17 @@ public class IA_Totem : InteractionArea
     public override void OnEndInteraction()
     {
         PlayerPickupAndThrow player = CustomNetworkManager.GetLocalPlayer().GetComponent<PlayerPickupAndThrow>();
-        TotemPiece piece = player.heldItem.GetComponent<TotemPiece>();
-        if (player.heldItem != null && piece != null) //we can match this more specifically later on
+        if (player.heldItem != null) //we can match this more specifically later on
         {
-            if (group == TotemPieceGroup.Any || group == piece.group)
+            TotemPiece piece = player.heldItem.GetComponent<TotemPiece>();
+
+            if (piece != null)
             {
-                player.DestroyItem();
-                CmdBuildTotem();
+                if (group == TotemPieceGroup.Any || group == piece.group)
+                {
+                    player.DestroyItem();
+                    CmdBuildTotem();
+                }
             }
         }
 
@@ -51,6 +56,14 @@ public class IA_Totem : InteractionArea
     [Command]
     public void CmdBuildTotem()
     {
+        RpcBuildTotem(currentVisualStage);
+
         currentVisualStage++;
+    }
+
+    [ClientRpc]
+    public void RpcBuildTotem(int stage)
+    {
+        Instantiate(smokePoof, visuals[stage].transform.position, Quaternion.identity);
     }
 }
