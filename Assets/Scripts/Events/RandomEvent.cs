2922eb9c9cfe35150f8489e511ab87f4c28917af
Pelle Bruinsma <Spookezatemyneighbours@gmail.com>
2021-04-22 15:22:52 +0200

    COCONUTPOCALYPSEEEEEEEEE

---
+++ Assets/Scripts/Events/RandomEvent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class RandomEvent : NetworkBehaviour
+{
+    public virtual void ServerEvent()
+    {
+        
+    }
+
+    public virtual void ClientEvent()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Events/RandomEventSystem.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class RandomEventSystem : NetworkBehaviour
+{
+    public Vector2 eventDelayRange = new Vector2(30f, 120f);
+
+    private bool started = false;
+    private MatchManager matchManager;
+    private RandomEvent[] events;
+
+    private void Start()
+    {
+        if (!isServer) //server only behaviour
+            return;
+
+        matchManager = FindObjectOfType<MatchManager>();
+    }
+
+    private void Update()
+    {
+        if (!isServer) //server only behaviour
+            return;
+
+        if (matchManager.gameStarted && !started)
+        {
+            //get all events
+            events = FindObjectsOfType<RandomEvent>();
+
+            //start coroutine
+            StartCoroutine(EventLoop());
+
+            started = true;
+        }
+    }
+
+    private IEnumerator EventLoop()
+    {
+        while (true)
+        {
+            //first wait
+            yield return new WaitForSeconds(Random.Range(eventDelayRange.x, eventDelayRange.y));
+
+            //start event
+            StartEvent(Random.Range(0, events.Length));
+        }
+    }
+
+    private void StartEvent(int eventIndex)
+    {
+        //invoke on server
+        events[eventIndex].ServerEvent();
+
+        //invoke on client
+        RpcStartClientEvent(eventIndex);
+    }
+
+    [ClientRpc]
+    private void RpcStartClientEvent(int eventIndex)
+    {
+        events[eventIndex].ClientEvent();
+    }
+}
