cd8229ca0f5d54578b7c68b95a462228dadbe872
Pelle Bruinsma <Spookezatemyneighbours@gmail.com>
2021-04-23 14:08:40 +0200

    Fucking meta files and coconut text

---
+++ Assets/Scripts/Misc/DestroyAfter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfter : MonoBehaviour
+{
+    public float seconds = 10f;
+
+    private void Start()
+    {
+        Destroy(gameObject, seconds);
+        Destroy(this);
+    }
+}
