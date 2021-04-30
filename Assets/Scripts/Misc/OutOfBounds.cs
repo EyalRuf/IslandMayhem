ce90acb48b4dd0a7b5793213dcbed62522813bea
Pelle Bruinsma <Spookezatemyneighbours@gmail.com>
2021-04-30 21:20:24 +0200

    Totem sound and respawns

---
+++ Assets/Scripts/Misc/OutOfBounds.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutOfBounds : MonoBehaviour
+{
+    public Vector3 respawnHeight;
+    public LayerMask objectMask;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(((1 << other.gameObject.layer) & objectMask) != 0 && other.GetComponent<Rigidbody>() != null)
+        {
+            other.transform.position = new Vector3(other.transform.position.x, respawnHeight.y, other.transform.position.z);
+        }
+    }
+}
