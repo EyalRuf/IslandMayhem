8646e513fede3edc28c11cf7cef36d679d8ac522
Pelle Bruinsma <Spookezatemyneighbours@gmail.com>
2021-05-19 14:47:55 +0200

    Buncha campsteps

---
+++ Assets/Scripts/Items&Interactinos/ExplorationCamps/CampStepButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CampStepButton : CampStep
+{
+    [Header("Button")]
+    public float duration;
+    public Transform overlapPosition;
+    public float overlapRadius;
+    public LayerMask overlapMask;
+
+    private void FixedUpdate()
+    {
+        if (!isServer)
+            return;
+
+        Collider[] colliders = Physics.OverlapSphere(overlapPosition.position, overlapRadius, overlapMask);
+
+        if (!isCompleted && colliders.Length > 0)
+        {
+            StartCoroutine(PressButton());
+        }
+    }
+
+    private IEnumerator PressButton()
+    {
+        CompleteStep();
+
+        yield return new WaitForSeconds(duration);
+
+        ResetStep();
+
+        yield return null;
+    }
+}
