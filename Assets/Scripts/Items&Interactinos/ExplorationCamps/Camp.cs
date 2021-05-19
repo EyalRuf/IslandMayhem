039c205c3ea442edc6f20cb0d06d2335386af5c4
Eyal Rufeisen <eyalrocks@gmail.com>
2021-05-19 13:27:33 +0200

    Camps LETS GOOO

---
+++ Assets/Scripts/Items&Interactinos/ExplorationCamps/Camp.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+using Mirror;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using System;
+
+public class Camp : NetworkBehaviour
+{
+    public List<CampStep> campSteps;
+    public GameObject rewardPrefab;
+
+    [SyncVar]
+    public int currStepsCompleted;
+
+    [SyncVar]
+    public bool isCoolingDown;
+    [SyncVar]
+    public float cooldownTimer;
+    public float cooldownDuration;
+
+    [SyncVar]
+    public int spawnsLeft;
+    public Vector2Int spawnLimitRange;
+
+    [Header("UI")]
+    public Text text;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (!isServer)
+            return;
+
+        campSteps = new List<CampStep>(GetComponentsInChildren<CampStep>());
+        spawnsLeft = UnityEngine.Random.Range(spawnLimitRange.x, spawnLimitRange.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = spawnsLeft <= 0 ? "Depleted" :isCoolingDown ? TimeFormatter.Format(cooldownTimer) : GetStepsText();
+        
+        if (!isServer && spawnsLeft <= 0)
+            return;
+
+        currStepsCompleted = campSteps.FindAll(step => step.isCompleted).Count;
+
+        if (isCoolingDown)
+        {
+            cooldownTimer -= Time.deltaTime;
+            if (cooldownTimer <= 0)
+            {
+                isCoolingDown = false;
+                ResetCamp();
+            }
+        } else
+        {
+            if (currStepsCompleted == campSteps.Count)
+            {
+                CampDone();
+            }
+        }
+    }
+
+    private string GetStepsText()
+    {
+        string stepsText = "";
+
+        campSteps.ForEach(step => stepsText += step.isCompleted ? " ✓ " : " X ");
+
+        return stepsText;
+    }
+
+    private void CampDone()
+    {
+        GameObject spawned = Instantiate(rewardPrefab, transform.position + (Vector3.up * 3), Quaternion.identity);
+        NetworkServer.Spawn(spawned);
+
+        isCoolingDown = true;
+        cooldownTimer = cooldownDuration;
+        spawnsLeft--;
+    }
+
+    void ResetCamp ()
+    {
+        isCoolingDown = false;
+        campSteps.ForEach(step => step.ResetStep());
+    }
+}
---
+++ Assets/Scripts/Items&Interactinos/ExplorationCamps/CampStep.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using Mirror;
+
+public class CampStep : NetworkBehaviour
+{
+    [SyncVar]
+    public bool isCompleted;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public virtual void CompleteStep ()
+    {
+        isCompleted = true;
+    }
+
+    public virtual void ResetStep ()
+    {
+        isCompleted = false;
+    }
+}
