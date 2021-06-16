b67eac26d358ef437697e864b79451cf1a38a46e
Pelle Bruinsma <Spookezatemyneighbours@gmail.com>
2021-06-16 10:55:10 +0200

    Size sync fix

--- Assets/Scripts/Player/NetworkPlayer.cs
+++ Assets/Scripts/Player/NetworkPlayer.cs
@@ -39,6 +39,8 @@ public class NetworkPlayer : NetworkBehaviour
     public Quaternion netPlayerRot;
     [SyncVar]
     public Vector3 netPlayerVel;
+    [SyncVar]
+    public Vector3 netPlayerSize;
     public float lerpFactor;
 
     void Start()
@@ -47,6 +49,7 @@ public class NetworkPlayer : NetworkBehaviour
         netPlayerPos = transform.position;
         netPlayerRot = transform.rotation;
         netPlayerVel = rb.velocity;
+        netPlayerSize = transform.localScale;
         localPlayerUI.SetActive(isLocalPlayer);
         remotePlayerUI.SetActive(!isLocalPlayer);
 
@@ -109,6 +112,8 @@ public class NetworkPlayer : NetworkBehaviour
                 transform.rotation = Quaternion.Lerp(transform.rotation, netPlayerRot, lerpFactor);
             if (Vector3.Distance(rb.velocity, netPlayerVel) > 0.01f)
                 rb.velocity = Vector3.Lerp(rb.velocity, netPlayerVel, lerpFactor);
+            if(Vector3.Distance(transform.localScale, netPlayerSize) > 0.01f)
+                transform.localScale = Vector3.Lerp(transform.localScale, netPlayerSize, lerpFactor);
         }
     }
 
@@ -120,7 +125,7 @@ public class NetworkPlayer : NetworkBehaviour
         np.netPlayerPos = pos;
         np.netPlayerRot = rot;
         np.netPlayerVel = vel;
-        np.transform.localScale = size;
+        np.netPlayerSize = size;
     }
 
     public override void OnStartClient()
